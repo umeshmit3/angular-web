e5cd95aac02e379720ce1fb6160fe198e1e830a3
umeshmit3 <umesh.mit3@gmail.com>
2018-10-01 00:28:19 +0530

    Introducing template files

---
+++ src/app/footer/app.footerComponent.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-footer',
+    templateUrl: './app.footerComponent.html',
+    styleUrls: ['./app.footerComponent.css']
+  })
+
+  export class FooterComponent {
+
+  }
---
+++ src/app/header/app.headerComponent.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './app.headerComponent.html',
+    styleUrls: ['./app.headerComponent.css']
+  })
+
+  export class HeaderComponent {
+
+  }
